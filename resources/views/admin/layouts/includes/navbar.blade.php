be10fe4bd9d1fb817dd53a3d1cc3881bd9d12454
Youssef Tharwat <yousseftharwat97@gmail.com>
2019-09-20 18:46:23 +0800

    logout fix

--- resources/views/admin/layouts/includes/navbar.blade.php
+++ resources/views/admin/layouts/includes/navbar.blade.php
@@ -6,18 +6,26 @@
         <ul class="navbar-nav border-left flex-row ">
 
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-nowrap px-3" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                    <img class="user-avatar rounded-circle mr-2" src="https://api.adorable.io/avatars/285/{{Auth::user()->name}}" alt="User Avatar">
+                <a class="nav-link dropdown-toggle text-nowrap px-3" data-toggle="dropdown" href="#" role="button"
+                   aria-haspopup="true" aria-expanded="false">
+                    <img class="user-avatar rounded-circle mr-2"
+                         src="https://api.adorable.io/avatars/285/{{Auth::user()->name}}" alt="User Avatar">
                     <span class="d-none d-md-inline-block">{{Auth::user()->name}}</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-small">
-                    <a class="dropdown-item text-danger" href="{{url('/logout')}}">
-                        <i class="material-icons text-danger">&#xE879;</i> Logout </a>
+                    <form method="post" action="{{route('logout')}}" style="cursor: pointer;" class="p-3">
+                        @csrf
+                        <button style="background: transparent;
+    border: none;" type="submit"><i class="material-icons text-danger">&#xE879;</i>
+                            Logout
+                        </button>
+                    </form>
                 </div>
             </li>
         </ul>
         <nav class="nav">
-            <a href="#" class="nav-link nav-link-icon toggle-sidebar d-md-inline d-lg-none text-center border-left" data-toggle="collapse" data-target=".header-navbar" aria-expanded="false" aria-controls="header-navbar">
+            <a href="#" class="nav-link nav-link-icon toggle-sidebar d-md-inline d-lg-none text-center border-left"
+               data-toggle="collapse" data-target=".header-navbar" aria-expanded="false" aria-controls="header-navbar">
                 <i class="material-icons">&#xE5D2;</i>
             </a>
         </nav>
